7d75d4f4af1bca400ab6918797d0a538a02bf023
LonelyLiaR <lonelyliar8523@gmail.com>
2017-04-25 17:48:22 +0800

    First commit: 2017.04.25

---
+++ README.md
@@ -0,0 +1,84 @@
+## hexo-theme-nameless
+Seen once, remember more. :clap:　[Priview](https://lonelyliar.github.io/)  
+
+![Priview](http://i4.buimg.com/4851/d9ceb071e8366baa.png)
+
+#### 关于主题：
+- 本主题不适合新手使用，使用前请确保具有前端基础.
+- Twitter风格的banner.
+- 全站Pjax. 上方的加载条仿写了[NProgress](https://github.com/rstacruz/nprogress).
+- 支持自适应，安卓移动端体验较差，后续~~随缘~~优化.
+- 已内置[Disqus评论系统](https://disqus.com)，需在站点根目录下的`_config.yml`设置`disqus_shortname`.
+- 兼容问题还没测试过... 如有问题请提[issues](https://github.com/lonelyliar/hexo-theme-nameless/issues).
+
+<br>
+## Version
+v1.0
+
+<br>
+## Quick Start
+#### Install
+
+`$ git clone https://github.com/lonelyliar/hexo-theme-nameless themes/nameless`
+
+<br>
+#### Deploy
+
+修改站点根目录下的`_config.yml`中的`theme`值为`nameless`.
+
+<br>
+#### Update
+
+	cd theme/nameless  
+	git pull
+
+## 配置
+#### 主题配置
+
+主题配置文件在主题目录下的`_config.yml`，请根据自己需要修改使用。  
+
+	# 导航. 显示名对应主题目录下的languages配置. value为跳转url.
+ 	nav:
+    	home: '/'
+    	archives: '/archives'
+    	tags: '/tags'
+    	links: '/links'
+    	about: '/about'
+
+  	# 标签. key为标签路由, value为标签名.
+    # 由于本人不喜欢路由里出现中文, 但标签名又得有中文.
+  	tags:
+    	example: 例子
+
+  	# 社交网络. 显示名对应主题目录下的languages配置. value为跳转url.
+  	socials:
+    	github: https://github.com/LonelyLiaR
+    	twitter: https://twitter.com/LonelyLiaR8523
+    	zhihu: https://www.zhihu.com/people/loleon-8523
+    	cloudmusic: http://music.163.com/#/user/home?id=64860394
+        weibo: http://weibo.com/lonelyliar8523
+
+  	# 友链. key为代号.
+    # value中: nickname为昵称; desc为概述; url为友链地址; banner(横幅)和avatar(头像)均为Boolean值, true时显示自主设置的图片, false时显示默认样式.
+    # 自主设置的banner和avatar需在站点根目录下的links文件夹下的index文件夹里面新建一个文件夹存放一张banner.png和avatar.png. 新建的文件夹名与key一样.
+    # 如下面的例子, key为example, 所以在站点根目录下的links文件夹下的index文件夹里新建一个example文件, 并放入banner.png和avatar.png就完成自主设置了.
+  	friends:
+    	example: { nickname: 'nickname', desc: 'desc', url: 'https://www.google.com/', banner: false, avatar: false }
+
+  	# 显示的icon. favicon为网页图标. appleicon为iOS系统上的Safari设为桌面程序是显示的图标.
+  	favicon: /favicon.ico
+  	appleicon: /apple.ico
+
+<br>
+#### 标签, 友链和关于页面的配置
+
+请在站点根目录下的`source`文件夹中新建文件夹`tags`、`links`、`about`.  
+并在各个文件夹中新建一个`index.md`, 设置`layout`属性为该文件夹的名字.  
+关于页的内容请在`about`文件夹下的`index.md`中进行编写.
+
+<br>
+#### 语言配置
+
+现只支持两种语言. 简体中文与英文. 默认为英文.  
+如需切换请修改站点根目录下的`_config.yml`中`language`的值.  
+语言配置文件在主题目录下的`languages`的文件夹内，请根据自己需要修改使用。
---
+++ source/js/nameless-script.js
@@ -0,0 +1,211 @@
+﻿
+'use strict';
+(function(global) {
+    //  Self introduction.
+    (function() {
+        var a = '─────────▄──────────────▄\r\n' +
+            '────────▌▒█───────────▄▀▒▌\r\n' +
+            '────────▌▒▒▀▄───────▄▀▒▒▒▐\r\n' +
+            '───────▐▄▀▒▒▀▀▀▀▄▄▄▀▒▒▒▒▒▐\r\n' +
+            '─────▄▄▀▒▒▒▒▒▒▒▒▒▒▒█▒▒▄█▒▐\r\n' +
+            '───▄▀▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▀██▀▒▌\r\n' +
+            '──▐▒▒▒▄▄▄▒▒▒▒▒▒▒▒▒▒▒▒▒▀▄▒▒▌\r\n' +
+            '──▌▒▒▐▄█▀▒▒▒▒▄▀█▄▒▒▒▒▒▒▒█▒▐\r\n' +
+            '─▐▒▒▒▒▒▒▒▒▒▒▒▌██▀▒▒▒▒▒▒▒▒▀▄▌\r\n' +
+            '─▌▒▀▄██▄▒▒▒▒▒▒▒▒▒▒▒░░░░▒▒▒▒▌\r\n' +
+            '─▌▀▐▄█▄█▌▄▒▀▒▒▒▒▒▒░░░░░░▒▒▒▐\r\n' +
+            '▐▒▀▐▀▐▀▒▒▄▄▒▄▒▒▒▒▒░░░░░░▒▒▒▒▌\r\n' +
+            '▐▒▒▒▀▀▄▄▒▒▒▄▒▒▒▒▒▒░░░░░░▒▒▒▐\r\n' +
+            '─▌▒▒▒▒▒▒▀▀▀▒▒▒▒▒▒▒▒░░░░▒▒▒▒▌\r\n' +
+            '─▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▐\r\n' +
+            '──▀▄▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▄▒▒▒▒▌\r\n' +
+            '────▀▄▒▒▒▒▒▒▒▒▒▒▄▄▄▀▒▒▒▒▄▀\r\n' +
+            '───▐▀▒▀▄▄▄▄▄▄▀▀▀▒▒▒▒▒▄▄▀\r\n' +
+            '──▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▀▀      如果你对我的博客网站有好的建议或者感兴趣的话, 欢迎联系我。\r\n';
+        console.log('%c' + a, 'color:#2DB4D8;');
+    })();
+    //  All links binding PJAX.
+    (function() {
+        _Pjax('a:not([target])', '.content-container');
+        if (_Ls('.nav a')) {
+            _LS(_Ls('.nav a')).forEach(function(that) {
+                that.onclick = function() {
+                    _LS(_Ls('.nav a.active')).removeClass('active');
+                    _LS(this).addClass('active');
+                };
+            });
+        };
+    })();
+
+    //  Some trick.
+    (function() {
+        LProgress.afterDone = changeActive;
+        _Ls('.back-to-top .button').onclick = function(e) {
+            var T = setInterval(function() {
+                document.body.scrollTop = document.body.scrollTop - 100;
+                document.body.scrollTop < 0 ? document.body.scrollTop = 0 : '';
+                document.body.scrollTop == 0 ? clearTimeout(T) : '';
+            }, 10);
+        };
+        _Ls('.back-to-top .button').onmouseover = function() {
+            if (navigator.userAgent.indexOf('Trident') != -1 || navigator.userAgent.indexOf('Edge') != -1) {
+                _Ls('.back-to-top .tip').style.cssText = 'display: block; opacity: 1; visibility: visible';
+                console.log(_Ls('.back-to-top .tip').style.cssText);
+                this.onmouseout = function() {
+                    _Ls('.back-to-top .tip').style.cssText = 'display: none; opacity: 0; visibility: hidden';
+                };
+            } else {
+                _LS(_Ls('.back-to-top .tip')).fadeIn(300);
+                this.onmouseout = function() {
+                    _LS(_Ls('.back-to-top .tip')).fadeOut(300);
+                };
+            };
+        };
+        _LS(_Ls('.social-networks .item')).forEach(function(item) {
+            item.onmouseover = function() {
+                if (_LS(_LS(this).parent()).child('.tip')) {
+                    var tip = _LS(_LS(this).parent()).child('.tip');
+                    _LS(tip).fadeIn(300);
+                    this.onmouseout = function() {
+                        _LS(tip).fadeOut(300);
+                    };
+                };
+            };
+        });
+        _LS(global).on('resize', adapt);
+
+        function adapt() {
+            if (window.innerWidth <= 600) {
+                if (!_Ls('.nav-container .nav-button')) {
+                    _Create('span', {
+                        'class': 'nav-button'
+                    }, _Ls('.nav-container'));
+                };
+                _Ls('.nav-container .nav-button').onclick = function(e) {
+                    _LS(this).toggleClass('active');
+                    _LS(_Ls('.nav-container .nav')).toggleClass('open');
+                    SB();
+                };
+                document.onclick = function() {
+                    !_LS(_Ls('.nav-container .nav-button')).hasClass('active') ? {} : _LS(_Ls('.nav-container .nav-button')).removeClass('active');
+                    !_LS(_Ls('.nav-container .nav')).hasClass('open') ? {} : _LS(_Ls('.nav-container .nav')).removeClass('open');
+                };
+            } else {
+                if (_Ls('.nav-container .nav .nav-button')) {
+                    _LS(_Ls('.nav-container .nav .nav-button')).remove();
+                };
+                !_LS(_Ls('.nav-container .nav')).hasClass('open') ? {} : _LS(_Ls('.nav-container .nav')).removeClass('open');
+            };
+        };
+        adapt();
+    })();
+
+    global.onpopstate = function(e) {
+        _Config.page = 1;
+    };
+    _LS(global).on('scroll', function(e) {
+        if ((document.body.scrollTop >= 256 && navigator.userAgent.indexOf('FireFox') == -1) || (document.documentElement.scrollTop >= 256 && navigator.userAgent.indexOf('Firefox') != -1)) {
+            !_LS(_Ls('.navbar-container')).hasClass('is-locked') ? (_LS(_Ls('.navbar-container')).addClass('is-locked'), _Ls('.content-container').style.cssText += 'margin-top: 49px;') : '';
+            if (_LS(_Ls('.back-to-top')).css('display') == 'none' || _LS(_Ls('.back-to-top')).css('visibility') == 'hidden') {
+                _Ls('.back-to-top').style.cssText += 'visibility: visible; opacity: 1; display: block';
+            };
+        } else {
+            !_LS(_Ls('.navbar-container')).hasClass('is-locked') ? '' : (_LS(_Ls('.navbar-container')).removeClass('is-locked'), _LS(_Ls('.content-container')).removeStyle('margin-top'));
+            if (_LS(_Ls('.back-to-top')).css('display') == 'block' && _LS(_Ls('.back-to-top')).css('visibility') == 'visible') {
+                _Ls('.back-to-top').style.cssText += 'visibility: hidden; opacity: 0; display: none';
+            };
+        };
+        if (reachBottom()) {
+            if (_Config.page == _Config.maxPage || (location.pathname != '/' && location.pathname != '/index.html')) {
+                return;
+            };
+            LProgress.start();
+            _Config.page += 1;
+            setTimeout(function() {
+                _Ajax({
+                    url: '/page/' + _Config.page + '/',
+                    success: function(r) {
+                        var container = _Create('html', {
+                            'innerHTML': r
+                        });
+                        var lm = _LS(_Ls('.articles-container .loading-more')).clone();
+                        _LS(_Ls('.articles-container .loading-more')).remove();
+                        if (_Config.page == _Config.maxPage) {
+                            _LS(lm).removeClass('loading-more').addClass('no-more');
+                            lm.innerHTML = '没有更多了.';
+                        };
+                        _Ls('.articles-container').innerHTML += _LS(container).child('.articles-container').innerHTML + lm.outerHTML;
+                        _Pjax('.content-container a:not([target])', '.content-container');
+                        _LS(_Ls('.content-container a:not([target])')).forEach(function(that) {
+                            that.onclick = function() {
+                                _Config.page = 1;
+                            };
+                        });
+                        LProgress.done();
+                    },
+                    error: function(error) {
+                        console.error('Failed to load !');
+                    }
+                });
+            }, 1000);
+        };
+
+        function reachBottom() {
+            var scrollTop = 0;
+            var clientHeight = 0;
+            var scrollHeight = 0;
+            if (document.documentElement && document.documentElement.scrollTop) {
+                scrollTop = document.documentElement.scrollTop;
+            } else if (document.body.scrollTop) {
+                scrollTop = document.body.scrollTop;
+            };
+            if (document.body.clientHeight && document.documentElement.clientHeight) {
+                clientHeight = (document.body.clientHeight < document.documentElement.clientHeight) ? document.body.clientHeight : document.documentElement.clientHeight;
+            } else {;
+                clientHeight = (document.body.clientHeight > document.documentElement.clientHeight) ? document.body.clientHeight : document.documentElement.clientHeight;
+            };
+            scrollHeight = Math.max(document.body.scrollHeight, document.documentElement.scrollHeight);
+            if (scrollTop + clientHeight >= scrollHeight - 110) {
+                return true;
+            } else {
+                return false;
+            };
+        };
+    });
+
+    function changeActive() {
+        if (/[^\d]/.test(window.location.pathname.replace('/', '').split('/')[0]) == false || window.location.pathname.replace(/\//g, '') == 'about') {
+            try {
+                if (_Ls('.comments-container') && _Ls('.comments-container').innerHTML == '') {
+                    DISQUS.reset({
+                        reload: true
+                    });
+                };
+            } catch (err) {
+                console.error('Disqus评论加载失败.');
+            };
+        };
+        if (_Ls('.nav a.active')) {
+            _LS(_Ls('.nav a.active')).removeClass('active');
+        };
+        switch (window.location.pathname.replace('/', '').replace(/\d/g, '').split('/')[0]) {
+            case '':
+                _LS(_Ls('.nav-item.home')).addClass('active');
+                break;
+            case 'archives':
+                _LS(_Ls('.nav-item.archives')).addClass('active');
+                break;
+            case 'tags':
+                _LS(_Ls('.nav-item.tags')).addClass('active');
+                break;
+            case 'links':
+                _LS(_Ls('.nav-item.links')).addClass('active');
+                break;
+            case 'about':
+                _LS(_Ls('.nav-item.about')).addClass('active');
+                break;
+            default:
+                break;
+        };
+    };
+})(this);
